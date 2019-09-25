b61a4ad59523f6113730eb9c20a0b213143821d6
Willian de Angelo <wdangelo1983@gmail.com>
2019-09-25 12:38:53 -0300

    Criados Atributos e instancias nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,15 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35): # se utilizar Alt+Enter o pycharm ja adiciona altomaticamente o atributo a Self
+        self.idade = idade
+        self.nome = nome #Atributo de passagem de parametros
     def cumprimentar(self):
         return f'Olá {id(self)}'
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano', idade=40)
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
-
+    print(p.nome, p.idade)
+    p.nome = 'willian' # passando o nome através da passagem de atributo nome
+    print(p.nome)
 
