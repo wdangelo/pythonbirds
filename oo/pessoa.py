99d5776646a62e4de4e3a53877e46b925338f412
Willian de Angelo <wdangelo1983@gmail.com>
2019-09-27 12:50:36 -0300

    Criado atributo dinamico e acessamos os atributos através do __dict__

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,7 +2,7 @@ class Pessoa:
     def __init__(self, *filhos, nome=None, idade=35): # se utilizar Alt+Enter o pycharm ja adiciona altomaticamente o atributo a Self
         self.idade = idade
         self.nome = nome #Atributo de passagem de parametros
-        self.filhos = list(filhos)
+        self.filhos = list(filhos)#atributo complexo
     def cumprimentar(self):
         return f'Olá {id(self)}'
 if __name__ == '__main__':
@@ -13,7 +13,9 @@ if __name__ == '__main__':
     print(luciano.cumprimentar())
     print(luciano.nome)
     print(luciano.idade)
-    print(luciano.filhos)
     for filho in luciano.filhos:
         print(filho.nome)
+    luciano.sobrenome = 'Ramalho' #atributo dinamico
+    print(luciano.__dict__)
+    print(renzo.__dict__)
 
