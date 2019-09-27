59c48a9f5f00f2581659a111e83d4f35032dc12f
Willian de Angelo <wdangelo1983@gmail.com>
2019-09-27 12:26:17 -0300

    Criado Atributo complexo "filhos"

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35): # se utilizar Alt+Enter o pycharm ja adiciona altomaticamente o atributo a Self
+    def __init__(self, *filhos, nome=None, idade=35): # se utilizar Alt+Enter o pycharm ja adiciona altomaticamente o atributo a Self
         self.idade = idade
         self.nome = nome #Atributo de passagem de parametros
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 if __name__ == '__main__':
-    p = Pessoa('Luciano', idade=40)
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome, p.idade)
-    p.nome = 'willian' # passando o nome através da passagem de atributo nome
-    print(p.nome)
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    print(luciano.filhos)
+    for filho in luciano.filhos:
+        print(filho.nome)
 
